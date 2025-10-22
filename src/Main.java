7cce621693a3b7bffdb37fa4b6a152e6503a5110
alus <benson940718@gmail.com>
2025-10-23 02:46:34 +0800

    add Tester.java to manage test function

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,22 @@ import utils.*;
 
 public class Main {
     public static void main(String[] args) {
-        System.err.println(BankCalendar.now());
+        Tester tester = new Tester();
 
-        CheckingAccount checking_account = new CheckingAccount("checking_account", 12.0);
-        System.out.println(checking_account.name());
+        tester.CheckingAccountNormalTest();
 
-        SavingAccount saving_account = new SavingAccount("saving_account", 12.0);
-        System.out.println(saving_account.name());
+        tester.SavingAccountNormalTest();
         
-        CDAccount cd_account = new CDAccount("saving_account", 12.0, 5000, 12);
-        System.out.println(cd_account.name());
-
-        LoanAccount loan_account = new LoanAccount("loan_account", 12.0, 1000);
-        System.out.println(loan_account.name());
+        tester.CDAccountNormalTest();
+        
+        tester.LoanAccountNormalTest();
+        
+        tester.CheckingAccountExceptionTest();
+        
+        tester.SavingAccountExceptionTest();
+        
+        tester.CDAccountExceptionTest();
+        
+        tester.LoanAccountExceptionTest();
     }
 }
---
+++ src/utils/Tester.java
@@ -0,0 +1,35 @@
+package utils;
+
+public class Tester {
+    public void CheckingAccountNormalTest() {
+
+    }
+    
+    public void SavingAccountNormalTest() {
+
+    }
+    
+    public void CDAccountNormalTest() {
+
+    }
+    
+    public void LoanAccountNormalTest() {
+
+    }
+    
+    public void CheckingAccountExceptionTest() {
+
+    }
+    
+    public void SavingAccountExceptionTest() {
+
+    }
+    
+    public void CDAccountExceptionTest() {
+
+    }
+    
+    public void LoanAccountExceptionTest() {
+
+    }
+}
